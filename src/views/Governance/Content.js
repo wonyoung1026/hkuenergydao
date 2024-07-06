8c7ab3165126e2143181295a9bfdd9a0850fe901
Won Jung <wonyoung1026@hotmail.com>
2024-07-07 00:29:07 +0800

    add link to polyscan

--- src/views/Governance/Content.js
+++ src/views/Governance/Content.js
@@ -2,14 +2,18 @@ import React, { Component, useState, useEffect } from "react"
 import StakeColumn from './StakeColumn.js'
 import RedeemColumn from './RedeemColumn.js'
 
-
-  
+const stakerAddress = process.env.REACT_APP_STAKER_CONTRACT_ADDRESS;
+const polygonscanLink = "https://cardona-zkevm.polygonscan.com/address/" + stakerAddress
 function Content() {
     return (
         <div className="container content">
             <div className="row">
                 <StakeColumn />
                 <RedeemColumn />
+            </div>
+            <div className="row" style={{minHeight: '100px'}}></div>
+            <div className="row">
+                    <i>Contract : <a href={polygonscanLink}>{stakerAddress}</a></i>
             </div>
         </div>
     );
