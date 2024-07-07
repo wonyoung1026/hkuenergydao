c2f61555cb837042b986b417f587d637d74f68ab
Won Jung <wonyoung1026@hotmail.com>
2024-07-07 17:27:44 +0800

    nits

--- src/views/GenerationStats/Content.js
+++ src/views/GenerationStats/Content.js
@@ -6,15 +6,6 @@ function Content() {
             <div className="container content">
                 <div className="row">
                     <div className="col-sm-6 talk">
-                        <br />
-                        <b>Please login using the below credential.</b>                                                       
-                        <br></br>
-                        rchan93@connect.hku.hk
-                        <br></br>
-                        vuYdftS9d8GL8gv
-                        <br></br>
-                        <br></br>
-                        As a member of the ENER DAO community member, you can able to view the real time generation and historical generation of the assets. At the moment, the solar PV test plant is connect via the Sungrow platform. 
                         <iframe src="https://web3.isolarcloud.com.hk/#/login" width="250%" height="1000" ></iframe>            
                         
 
